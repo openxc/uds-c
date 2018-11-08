10555aa82d8a94e956528439d301bce3323b88b1
pjt0620 <pjt0620@gmail.com>
2018-11-08 10:08:04 -0500

    Fix uds-c for wuninitialized compliance

--- src/uds/extras.c
+++ src/uds/extras.c
@@ -8,20 +8,20 @@ DiagnosticRequestHandle diagnostic_request_malfunction_indicator_status(
         DiagnosticMilStatusReceived callback) {
     // TODO request malfunction indicator light (MIL) status - request mode 1
     // pid 1, parse first bit
-    DiagnosticRequestHandle handle;
+    DiagnosticRequestHandle handle = {};
     return handle;
 }
 
 DiagnosticRequestHandle diagnostic_request_vin(DiagnosticShims* shims,
         DiagnosticVinReceived callback) {
-    DiagnosticRequestHandle handle;
+    DiagnosticRequestHandle handle = {};
     return handle;
 }
 
 DiagnosticRequestHandle diagnostic_request_dtc(DiagnosticShims* shims,
         DiagnosticTroubleCodeType dtc_type,
         DiagnosticTroubleCodesReceived callback) {
-    DiagnosticRequestHandle handle;
+    DiagnosticRequestHandle handle = {};
     return handle;
 }
 
@@ -34,6 +34,6 @@ DiagnosticRequestHandle diagnostic_enumerate_pids(DiagnosticShims* shims,
     // before calling the callback, split up the received bytes into 1 or 2 byte
     // chunks depending on the mode so the final pid list is actual 1 or 2 byte PIDs
     // TODO request supported PIDs  - request PID 0 and parse 4 bytes in response
-    DiagnosticRequestHandle handle;
+    DiagnosticRequestHandle handle = {};
     return handle;
 }
